ed0973be95a4464b1b951fa4ec75a8634530f985
karthikeyankkn <karthikeyan@fixnix.co>
2017-09-28 19:27:50 +0530

    modified refferal dashboard

--- app/modules/redeem_coupon/redeem_couponCtrl.js
+++ app/modules/redeem_coupon/redeem_couponCtrl.js
@@ -13,7 +13,7 @@
 		.module('redeemcoupon')
 		.controller('redeemcouponCtrl', RedeemCoupon);
 
-	RedeemCoupon.$inject = ['redeemcouponService'];
+	RedeemCoupon.$inject = ['redeemcouponService', '$http'];
 
 	/*
 	* recommend
@@ -21,11 +21,23 @@
 	* and bindable members up top.
 	*/
 
-	function RedeemCoupon(redeemcouponService) {
+	function RedeemCoupon(redeemcouponService, $http) {
 		/*jshint validthis: true */
 		var vm = this;
-		vm.title = "Hello, angular-app!";
-		vm.version = "1.0.0";
+		vm.validatecode = function(){
+			debugger
+			var output = [];
+			vm.redemption = [];
+			vm.redemption.amount = vm.amount;
+			vm.redemption.coupon_code = vm.coupon_code;
+			vm.redemption.business_id = 1;
+			output.redemption = vm.redemption;
+			console.log(output);
+			$http.post('https://api.spini.co/v1/redemptions',{"redemption":{"coupon_code":"7ABE9B", "business_id": "1", "amount": "1000"}}, {
+				  headers: { 'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MDY2MDM1MTksInN1YiI6MTB9.yUwqTPfXKbU5gsHT7mnOcx2StTsmLd1F5KmyCowhYq4' }
+				  // params: { bookId: 42 }
+				});
+		}
 		vm.listFeatures = redeemcouponService.getFeaturesList();
 
 	}
